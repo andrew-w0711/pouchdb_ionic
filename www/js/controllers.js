0aacd84ee09eb3ae2cb7f5fc8e5abfd7257991ae
webmas216 <antonpavlov1112@gmail.com>
2015-11-18 02:09:55 -0500

    Ionic + PouchDB ( for internet connection is not available )

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,7 @@ angular.module('starter.controllers', [])
   $scope.score = {};
 
   $q.all([
-    pp.getScore('1'),
+    pp.getScore('1')
     pp.getScore('2'),
     pp.getScore('3')
   ]).then(function(res) {
@@ -48,62 +48,4 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
-});
-
-var services = angular.module('starter.services', []);
-
-services.value('version', '0.1');
-
-services.factory('pouchdb', function() {
-  PouchDB.enableAllDbs = true;
-  var database = new PouchDB('starter221');
-  return database;
-});
-
-
-services.factory('pp', function($q, pouchdb, $rootScope) {
-
-  return {
-    add: function(playerId) {
-      var deferred = $q.defer();
-      var doc = {
-        type: 'goal',
-        playerId: playerId
-      };
-      pouchdb.post(doc, function(err, res) {
-        $rootScope.$apply(function() {
-          if (err) {
-            deferred.reject(err)
-          } else {
-            deferred.resolve(res)
-          }
-        });
-      });
-      return deferred.promise;
-    },
-    getScore: function(playerId) {
-
-      var deferred = $q.defer();
-      var map = function(doc) {
-        if (doc.type === 'goal') {
-          emit(doc.playerId, null)
-        }
-      };
-      pouchdb.query({map: map, reduce: '_count'}, {key: playerId}, function(err, res) {
-        $rootScope.$apply(function() {
-          if (err) {
-            deferred.reject(err);
-          } else {
-            if (!res.rows.length) {
-              deferred.resolve(0);
-            } else {
-              deferred.resolve(res.rows[0].value);
-            }
-          }
-        });
-      });
-      return deferred.promise;
-    }
-  }
-
 });
